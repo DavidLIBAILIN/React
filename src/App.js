aca1536b0964c032c7b4cad73373f72de5f520b2
Bolin Li <davidlibailin@gmail.com>
2024-12-25 17:41:17 -0500

    Zustand Demo

--- src/App.js
+++ src/App.js
@@ -2,27 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 // zustand
 import { create } from 'zustand'
+import {useEffect} from "react";
+const URL = 'http://geek.itheima.net/v1_0/channels'
 
-// 1.创建Store
-//
-const useStore = create((set)=>{
+// 1. 拆分子模块 再组合起来
+const createCounterStore = (set)=> {
   return {
     // 状态数据
     count: 0,
     // 修改状态数据的方法
-    inc: ()=>{
-      set((state)=>({ count: state.count + 1}))
+    inc: () => {
+      set((state) => ({count: state.count + 1}))
+    },
+  }
+}
+
+const createChannelStore = (set)=>{
+  return {
+  channelList: [],
+      fetchGetList: async ()=> {
+      const res = await fetch(URL)
+      const jsonRes = await res.json()
+      console.log(jsonRes)
+      set({
+      channelList: jsonRes.data.channels
+      })
     }
+  }
+}
+// 2. 组件使用
+
+const useStore = create((...a)=>{
+  return {
+    ...createCounterStore(...a),
+    ...createChannelStore(...a)
   }
 })
 
 // 2. 绑定Store到组件
 // useStore => { count, inc }
 function App() {
-  const { count, inc } = useStore()
+  const { count, inc, fetchGetList, channelList } = useStore()
+  useEffect(() => {
+    fetchGetList()
+  }, [fetchGetList]);
   return (
     <>
       <button onClick={inc}>{count}</button>
+      <ul>
+        {
+          channelList.map(item=><li key={item.id}>{item.name}</li>)
+        }
+      </ul>
     </>
   );
 }
